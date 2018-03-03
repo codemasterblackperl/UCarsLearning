4b80985743defa12d4f99bfd42f06dc91f6d5726
Ajit <codemasterblackperl@outlook.com>
2018-03-03 21:03:47 +0530

    added service type create and index view. Also added a new template for small button

---
+++ UAutoServiceLearn/Controllers/ServiceTypesController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UAutoServiceLearn.Data;
+using UAutoServiceLearn.Models;
+
+namespace UAutoServiceLearn.Controllers
+{
+    public class ServiceTypesController : Controller
+    {
+        protected readonly ApplicationDbContext _db;
+
+        public ServiceTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        //Get: ServiceTypes/Index
+        public IActionResult Index()
+        {
+            return View(_db.ServiceTypes.ToList());
+        }
+
+        //Get: ServiceTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //post: ServiceTypes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ServiceType serviceType)
+        {
+            if (!ModelState.IsValid)
+                return View(serviceType);
+
+            await _db.ServiceTypes.AddAsync(serviceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+
+        //Get: ServiceTypes/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+
+            var service = await _db.ServiceTypes.FindAsync(id);
+            if(service==null)
+                return RedirectToAction("Index");
+
+            return View(service);
+        }
+
+        //post: ServiceTypes/Edit
+        [HttpPost]
+        public async Task<IActionResult> Edit(ServiceType serviceType)
+        {
+            if (!ModelState.IsValid)
+                return View(serviceType);
+
+            var service = await _db.ServiceTypes.FindAsync(serviceType.Id);
+            service.Name = serviceType.Name;
+
+            _db.ServiceTypes.Update(service);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        //get: ServiceTypes/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+
+            var service = await _db.ServiceTypes.FindAsync(id);
+            if(service==null)
+                return RedirectToAction("Index");
+
+            return View(service);
+        }
+
+        //post: ServiceTypes/Delete
+        [HttpPost]
+        public async Task<IActionResult> Delete(ServiceType serviceType)
+        {
+            //var service = await _db.ServiceTypes.FindAsync(serviceType.Id);
+            _db.ServiceTypes.Remove(serviceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ UAutoServiceLearn/Models/VisualHelpers/SmallButtonPartial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UAutoServiceLearn.Models
+{
+    public class SmallButtonPartial
+    {
+        public string ButtonStyleType { get; set; }
+        public string ActionName { get; set; }
+        public string GlyphIcon { get; set; }
+        public string Text { get; set; }
+
+        public int? ServiceId { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                var param = "";
+                if(ServiceId!=null&&ServiceId!=0)
+                {
+                    param= @"/" + $"{ServiceId}";
+                }
+                return param;
+            }
+        }
+    }
+}
